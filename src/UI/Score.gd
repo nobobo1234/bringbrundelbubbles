26561ae523518ded778c465bc60c1111d8f8d17b
nobobo1234 <noahvanboven@gmail.com>
2020-03-15 16:11:56 +0100

    Adding icon and pause screen

--- src/UI/Score.gd
+++ src/UI/Score.gd
@@ -5,8 +5,10 @@ onready var leveltext: = $Level
 onready var total_bottles: = get_tree().get_nodes_in_group("flessen").size()
 onready var pause_title: Label = get_node("DiedScreen/Label")
 onready var scene_tree: = get_tree()
-onready var pause_overlay: ColorRect = get_node("DiedScreen")
+onready var died_overlay: ColorRect = get_node("DiedScreen")
+onready var pause_overlay: ColorRect = get_node("PauseScreen")
 
+var died: = false setget set_died
 var paused: = false setget set_paused
 
 # Called when the node enters the scene tree for the first time.
@@ -15,13 +17,13 @@ func _ready() -> void:
 	ScoreData.connect("level_updated", self, "update_interface")
 	ScoreData.connect("player_died", self, "_on_PlayerData_player_died")
 	update_interface()
-
-func update_interface() -> void:
-	score.text = "Flesjes: %s/%s" % [ScoreData.bottles, total_bottles]
-	leveltext.text = "Level: %s" % ScoreData.levels
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause"):
+		self.paused = true
 
 func _on_PlayerData_player_died() -> void:
-	self.paused = true
+	self.died = true
 
 func _on_Score_tree_entered() -> void:
 	total_bottles = get_tree().get_nodes_in_group("flessen").size()
@@ -29,11 +31,18 @@ func _on_Score_tree_entered() -> void:
 	ScoreData.bottles = 0
 	ScoreData.levels += 1
 	
+func set_died(value: bool) -> void:
+	score.visible = false
+	leveltext.visible = false
+	died = value
+	scene_tree.paused = value
+	died_overlay.visible = value
+	
 func set_paused(value: bool) -> void:
-	$Score.visible = false
-	$Level.visible = false
 	paused = value
 	scene_tree.paused = value
-	pause_overlay.visible = true
-	
+	pause_overlay.visible = value
 
+func update_interface() -> void:
+	score.text = "Flesjes: %s/%s" % [ScoreData.bottles, total_bottles]
+	leveltext.text = "Level: %s" % ScoreData.levels
