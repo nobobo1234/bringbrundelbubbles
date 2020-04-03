f98310291080908b8659dfff7c5fb3fea8488719
nobobo1234 <noahvanboven@gmail.com>
2020-04-03 21:14:44 +0200

    Small i18n fix

--- src/Screens/EndScreen.gd
+++ src/Screens/EndScreen.gd
@@ -3,4 +3,4 @@ extends ColorRect
 onready var label: = $Label
 
 func _ready() -> void:
-	label.text = label.text % [ScoreData.total_bottles_collected, ScoreData.tries]
+	label.text = tr(label.text) % [ScoreData.total_bottles_collected, ScoreData.tries]
